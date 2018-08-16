c457b8cc1844be6df047b7c5f72babcea9f57961
SejalZilpe <sejalzilpe2110@gmail.com>
2018-08-16 17:32:22 +0530

    this is first commit

---
+++ mat_mul.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+int main()
+{
+	int mat1[3][3],mat2[3][3],mat3[3][3],i,j,k;	
+	printf("\n enter 1st array : ");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&mat1[i][j]);
+		}
+	}
+	printf("\n enter 2nd array : ");
+	for(i=0;i<3;i++)
+        {
+        	for(j=0;j<3;j++)
+        	{
+                	scanf("%d",&mat2[i][j]);
+        	}
+        }
+	for(i=0;i<3;i++)
+        {
+        	for(j=0;j<3;j++)
+        	{
+                	mat3[i][j]=0;
+        	}
+        }
+	printf("\n multiplying the matrix : ");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			for(k=0;k<3;k++)
+			{
+	 			mat3[i][j]=mat3[i][j]+(mat2[i][k]*mat1[k][j]);	
+			}
+		}		
+	}
+	printf("\n the first matrix \n ");
+	for(i=0;i<3;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        printf("%d\t",mat1[i][j]);
+                }
+                printf("\n");
+        }
+	printf("\n the second matrix \n ");
+	for(i=0;i<3;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        printf("%d\t",mat2[i][j]);
+                }
+                printf("\n");
+        }
+	printf("\n the output is : \n ");
+	for(i=0;i<3;i++)
+        {
+        	for(j=0;j<3;j++)
+        	{
+                	printf("%d\t",mat3[i][j]);
+        	}
+		printf("\n");
+        }
+
+return 0;
+}
+
